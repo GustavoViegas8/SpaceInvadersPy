d4b3152ce10c1ef35bdb0e064f84b7a9ba0e8ebe
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-05-19 20:17:53 -0300

    :tada: Inicio do Projeto

---
+++ Space Invanders.py
@@ -0,0 +1,64 @@
+import pygame
+from alien import Alien
+from generator import Generator
+from rocket import Rocket
+from hero import Hero
+
+class Game:
+    screen = None
+    aliens = []
+    rockets = []
+    lost = False
+
+    def __init__(self, width, height):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+        done = False
+
+        hero = Hero(self, width / 2, height - 20)
+        generator = Generator(self)
+        rocket = None
+
+        while not done:
+            if len(self.aliens) == 0:
+                self.displayText("Você Venceu!")
+
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_LEFT]: 
+                hero.x -= 2 if hero.x > 20 else 0  
+            elif pressed[pygame.K_RIGHT]:  
+                hero.x += 2 if hero.x < width - 20 else 0  
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP and not self.lost:
+                    self.rockets.append(Rocket(self, hero.x, hero.y))
+
+            pygame.display.flip()
+            self.clock.tick(60)
+            self.screen.fill((0, 0, 0))
+
+            for alien in self.aliens:
+                alien.draw()
+                alien.checkCollision(self)
+                if (alien.y > height):
+                    self.lost = True
+                    self.displayText("Game Over")
+
+            for rocket in self.rockets:
+                rocket.draw()
+
+            if not self.lost: hero.draw()
+
+    def displayText(self, text):
+        pygame.font.init()
+        font = pygame.font.SysFont('Arial', 50)
+        textsurface = font.render(text, False, (44, 0, 62))
+        self.screen.blit(textsurface, (110, 160))
+
+if __name__ == '__main__':
+    game = Game(600, 400)
