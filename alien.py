df0e490013c34945286cdcf62a2fd5e90ddb1d67
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-05-19 20:20:55 -0300

    :alien: Classe Alien

---
+++ alien.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Alien:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.game = game
+        self.y = y
+        self.size = 30
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen,  
+                         (81, 43, 88),  
+                         pygame.Rect(self.x, self.y, self.size, self.size))
+        self.y += 0.05
+
+    def checkCollision(self, game):
+        for rocket in game.rockets:
+            if (rocket.x < self.x + self.size and
+                    rocket.x > self.x - self.size and
+                    rocket.y < self.y + self.size and
+                    rocket.y > self.y - self.size):
+                game.rockets.remove(rocket)
+                game.aliens.remove(self)
